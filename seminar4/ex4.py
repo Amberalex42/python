8bb124f5725491c21be85f0e9dbc0dbad2853bf7
Aleksey <contramoty@gmail.com>
2022-12-04 23:44:24 +0200

    Решение зедачи 4

--- seminar4/ex4.py
+++ seminar4/ex4.py
@@ -5,33 +5,28 @@
 # k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 import random
 
-# k = int(input("Введите значение степени k: "))
-# d = {i: random.randint(0, 1) for i in range(k, -1, -1)}
-koef = [0, 1, 0, 1, 1]
-d = {i: koef[i] for i in range(4, -1, -1)}
+k = int(input("Введите значение степени k: "))
+d = {i: random.randint(0, 100) for i in range(k, -1, -1)}
 filt_d = {k: v for k, v in d.items() if v != 0}
-print(filt_d)
-str_poly = ""
-if not len(filt_d):
-    str_poly = "0 = 0"
-elif len(filt_d) == 1 and 0 in filt_d:
-    str_poly = "Произошла ошибка при определении коэффициентов."
-else:
-    koef_list = []
+
+
+def get_string(d):
+    result = []
     for el in d:
-        if el != 1:
-            pass
+        if el > 1:
+            if d[el] > 1:
+                result.append(f"{d[el]}x^{el}")
+            elif filt_d[el] == 1:
+                result.append(f"x^{el}")
+        elif el == 1:
+            if filt_d[el] > 1:
+                result.append(f"{d[el]}x")
+            elif d[el] == 1:
+                result.append("x")
         else:
-            pass
-    print([f"{v}x^{k}" if v != 1 else f"x^{k}" for k, v in filt_d.items()])
-#     for el in filt_d:
-#         if el > 1:
-#             str_poly += f"{filt_d[el] if filt_d[el] != 1 else ''}x^{el} "
-#         elif el == 1:
-#             str_poly += f"+ {filt_d[el] if filt_d[el] > 1 else ''}x "
-#         else:
-#             str_poly += f"+ {filt_d[el]} "
-#     str_poly += "= 0"
-# print(str_poly)
-# with open("output_4.txt", "w", encoding="utf-8") as file:
-#     file.write(str_poly)
+            result.append(str(d[el]))
+    return result
+
+
+with open("output_4.txt", "w", encoding="utf-8") as file:
+    file.write(" + ".join(get_string(filt_d)) + " = 0")
