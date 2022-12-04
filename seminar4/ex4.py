759c8eab6184ff99662b42eb58bd40d9a4fb6af7
Aleksey <contramoty@gmail.com>
2022-12-04 02:24:18 +0200

    Дополнение решения задачи 4

--- seminar4/ex4.py
+++ seminar4/ex4.py
@@ -5,13 +5,33 @@
 # k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 import random
 
-k = int(input("Введите значение степени k: "))
-d = {i: random.randint(0, 100) for i in range(k, -1, -1)}
+# k = int(input("Введите значение степени k: "))
+# d = {i: random.randint(0, 1) for i in range(k, -1, -1)}
+koef = [0, 1, 0, 1, 1]
+d = {i: koef[i] for i in range(4, -1, -1)}
+filt_d = {k: v for k, v in d.items() if v != 0}
+print(filt_d)
 str_poly = ""
-for el in d:
-    if el != 0:
-        str_poly += f"{d[el]}x^{el} + "
-    else:
-        str_poly += f"{d[el]} = 0"
-with open("output_4.txt", "w", encoding="utf-8") as file:
-    file.write(str_poly)
+if not len(filt_d):
+    str_poly = "0 = 0"
+elif len(filt_d) == 1 and 0 in filt_d:
+    str_poly = "Произошла ошибка при определении коэффициентов."
+else:
+    koef_list = []
+    for el in d:
+        if el != 1:
+            pass
+        else:
+            pass
+    print([f"{v}x^{k}" if v != 1 else f"x^{k}" for k, v in filt_d.items()])
+#     for el in filt_d:
+#         if el > 1:
+#             str_poly += f"{filt_d[el] if filt_d[el] != 1 else ''}x^{el} "
+#         elif el == 1:
+#             str_poly += f"+ {filt_d[el] if filt_d[el] > 1 else ''}x "
+#         else:
+#             str_poly += f"+ {filt_d[el]} "
+#     str_poly += "= 0"
+# print(str_poly)
+# with open("output_4.txt", "w", encoding="utf-8") as file:
+#     file.write(str_poly)
