a59679e6bfb68feb671e6648d5c1cf0ecdc2c6df
malkovav <malkov@fakel.net>
2022-12-12 16:29:59 +0200

    Init seminar 8

---
+++ seminar8/main.py
@@ -0,0 +1,18 @@
+staff = [
+    {
+        "id": 1,
+        "name": "Петр",
+        "last_name": "Иванов",
+        "position": "инженер",
+        "salary": 55555,
+        "bonus": 33333
+    },
+    {
+        "id": 2,
+        "name": "Петр",
+        "last_name": "Петров",
+        "position": "инженер",
+        "salary": 55555,
+        "bonus": 33333
+    }
+]
